251478a6b64643c8d4a13857b2a15fd92cc26954
chederavikiran <chederavi44@gmail.com>
2025-07-08 17:28:46 +0530

    feat: Initialize portfolio project with React, Vite, and Tailwind CSS
    - Created package.json with dependencies and scripts for development and build.
    - Added main application structure in App.jsx.
    - Included background image and SVG assets for UI components.
    - Developed FlipWords component for animated text transitions.
    - Implemented HeroText component to display introductory text with animations.
    - Created ParallaxBackground component for a visually appealing background.
    - Styled application with index.css using Tailwind CSS utilities.
    - Set up main entry point in main.jsx for rendering the application.
    - Built Navbar component for navigation with responsive design.
    - Configured Vite for React and Tailwind CSS integration.

---
+++ src/components/ParallaxBackground.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ParallaxBackground = () => {
+  return (
+    <section className='absolute inset-0 bg-black/40'>
+        <div className='relative h-screen overflow-y-hidden'>
+    {/* background image */}
+    <div className="absolute inset-0 w-full h-screen -z-50"
+    style={{
+        backgroundImage:"url('/src/assets/backgroundimg.jpeg')",
+        backgroundPosition:"bottom",
+        backgroundSize:"cover",
+    }}/>
+       <div/>
+          <div/>
+             <div/>
+                <div/>
+                   <div/>
+
+        </div>
+        </section>
+  )
+}
+
+export default ParallaxBackground
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { motion } from 'motion/react';
+
+function Navigation(){
+    return <ul className='nav-ul'>
+        <li className='nav-li'>
+            <a href="Home">Home</a>
+        </li>
+         <li className='nav-li'>
+            <a href="About">About</a>
+        </li>
+         <li className='nav-li'>
+            <a href="Work">Work</a>
+        </li>
+         <li className='nav-li'>
+            <a href="Contact">Contact</a>
+        </li>
+    </ul>
+}
+
+
+const Navbar = () => {
+   const[isOpen, setIsOpen] =useState(false);
+
+  return (
+    <div className='fixed inset-x-0 z-20 backdrop-blur-lg bg-primary/40'>
+        <div className='mx-auto c-space max-w-7xl'>
+<div className='flex items-center justify-between py-2 sm:py-0'>
+    <a className='text-xl font-bold transition-colors
+     text-neutral-400 hover:text-white'>ravikiarn</a>
+     <button onClick={()=>{
+        setIsOpen(!isOpen)
+     }} className='flex cursor-pointer text-neutral-400
+      hover:text-white focus:outline-none sm:hidden'>
+        <img src={isOpen?"src/assets/close.svg":"src/assets/menu.svg"} alt="toggle"className='w-6 h-6'/>
+     </button>
+     <nav className='hidden sm:flex'>
+       < Navigation/>
+     </nav>
+</div>
+</div>
+{isOpen&&(<motion.div className='block overflow-hidden text-center sm:hidden'>
+<nav className='pb-5' initial={{opacity:0,x:-10}} 
+animate={{opacity:1,x:0}}
+style={{maxHeight:"100vh"}}
+transition={{duration:1}}>
+    <Navigation/>
+</nav></motion.div>)}
+    </div>
+  )
+}
+
+export default Navbar
